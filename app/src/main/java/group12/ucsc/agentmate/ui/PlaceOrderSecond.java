81958b68e8fc86454ed28c74328fcf999a22fd05
NickHub4017 <nrv.rox.u@gmail.com>
2014-09-16 15:19:50 +0530

    Add PlaceOrderSecond's putExtra methods Complete table creation

--- app/src/main/java/group12/ucsc/agentmate/ui/PlaceOrderSecond.java
+++ app/src/main/java/group12/ucsc/agentmate/ui/PlaceOrderSecond.java
@@ -27,43 +27,55 @@ public class PlaceOrderSecond extends Activity {
         final Representative logged_rep=(Representative)getIntent().getExtras().getSerializable("logged_user");
         final Vendor sel_vendor=(Vendor)getIntent().getExtras().getSerializable("vendor");
         TextView logged_vendor_tv=(TextView)findViewById(R.id.txt_vname_order_b);
-        logged_vendor_tv.setText(sel_vendor.getShopName());
+        logged_vendor_tv.setText("Selected Vendor is :- "+sel_vendor.getShopName());
         //TableLayout t1;
-        TableLayout tl = (TableLayout) findViewById(R.id.table1);
+
+
+        TableLayout tl = (TableLayout) findViewById(R.id.selected_table1);
         final TableRow tr_head = new TableRow(this);
         tr_head.setId(10);
-        tr_head.setBackgroundColor(Color.GRAY);
+        tr_head.setBackgroundColor(Color.BLACK);
         tr_head.setLayoutParams(new TableRow.LayoutParams(
                 TableRow.LayoutParams.MATCH_PARENT,
                 TableRow.LayoutParams.WRAP_CONTENT));
-        TextView label_date = new TextView(this);
-        label_date.setId(20);
-        label_date.setText("DATE");
-        label_date.setTextColor(Color.WHITE);
-        label_date.setPadding(5, 5, 5, 5);
+        TextView label_Item_ID = new TextView(this);
+        label_Item_ID.setId(20);
+        label_Item_ID.setText("Item ID");
+        label_Item_ID.setTextColor(Color.WHITE);
+        label_Item_ID.setPadding(5, 5, 5, 5);
+        tr_head.addView(label_Item_ID);// add the column to the table row here
 
-        tr_head.addView(label_date);// add the column to the table row here
-        tr_head.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                tr_head.getId();
-                Toast.makeText(getApplicationContext(),"--> "+tr_head.findViewById(20).getId(),Toast.LENGTH_SHORT).show();
-                TextView e=(TextView)tr_head.findViewById(20);
-                e.setText("***********");
-            }
-        });
-        TextView label_weight_kg = new TextView(this);
-        label_weight_kg.setId(21);// define id that must be unique
-        label_weight_kg.setText("Wt(Kg.)"); // set the text for the header
-        label_weight_kg.setTextColor(Color.WHITE); // set the color
-        label_weight_kg.setPadding(5, 5, 5, 5); // set the padding (if required)
-        tr_head.addView(label_weight_kg); // add the column to the table row here
+        TextView label_Item_Name = new TextView(this);
+        label_Item_Name.setId(21);// define id that must be unique
+        label_Item_Name.setText("Item Name"); // set the text for the header
+        label_Item_Name.setTextColor(Color.WHITE); // set the color
+        label_Item_Name.setPadding(5, 5, 5, 5); // set the padding (if required)
+        tr_head.addView(label_Item_Name); // add the column to the table row here
+
+        TextView label_Qty = new TextView(this);
+        label_Qty.setId(21);// define id that must be unique
+        label_Qty.setText("Qty"); // set the text for the header
+        label_Qty.setTextColor(Color.WHITE); // set the color
+        label_Qty.setPadding(5, 5, 5, 5); // set the padding (if required)
+        tr_head.addView(label_Qty); // add the column to the table row here
+
+        TextView label_Discount = new TextView(this);
+        label_Discount.setId(21);// define id that must be unique
+        label_Discount.setText("Discount"); // set the text for the header
+        label_Discount.setTextColor(Color.WHITE); // set the color
+        label_Discount.setPadding(5, 5, 5, 5); // set the padding (if required)
+        tr_head.addView(label_Discount); // add the column to the table row here
+
+        TextView label_Price = new TextView(this);
+        label_Price.setId(21);// define id that must be unique
+        label_Price.setText("Price"); // set the text for the header
+        label_Price.setTextColor(Color.WHITE); // set the color
+        label_Price.setPadding(5, 5, 5, 5); // set the padding (if required)
+        tr_head.addView(label_Price); // add the column to the table row here
 
         tl.addView(tr_head, new TableLayout.LayoutParams(
                 TableLayout.LayoutParams.MATCH_PARENT,
                 TableLayout.LayoutParams.WRAP_CONTENT));
-
-        
 
     }
 }
