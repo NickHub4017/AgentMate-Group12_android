8785618b7738af71257439e72f46ace8f286a24e
NickHub4017 <nrv.rox.u@gmail.com>
2014-09-20 00:40:23 +0530

    Qty dialog box data sync is completed but no error corrections. With over stock defence.

--- app/src/main/java/group12/ucsc/agentmate/ui/DialogGetQty.java
+++ app/src/main/java/group12/ucsc/agentmate/ui/DialogGetQty.java
@@ -1,8 +1,11 @@
 package group12.ucsc.agentmate.ui;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -51,13 +54,26 @@ public class DialogGetQty extends DialogFragment{
         txt_get_dmnd_qty=(EditText)view.findViewById(R.id.edit_demand_qty);
 
         TextView Remain_tv=(TextView)view.findViewById(R.id.text_remain);
+
         Remain_tv.setText("Remaining Stock for this product "+PlaceOrderSecond.cur_st_value);
+
+        unit_spinner=(Spinner)view.findViewById(R.id.unit_spinner);
+        unit_demand_spinner=(Spinner)view.findViewById(R.id.demand_unit_spinner);
+
         btn_submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 entered_qty=Integer.parseInt(txt_get_qty.getText().toString());
-                cm.onDialogMessage(entered_qty,0,unit_spinner.getSelectedItemPosition(),unit_demand_spinner.getSelectedItemPosition());//ToDo set arrays
-                dismiss();
+                if (entered_qty>PlaceOrderSecond.cur_st_value){
+                    show_warning();
+                }
+
+                else{
+                    cm.onDialogMessage(entered_qty,0,unit_spinner.getSelectedItemPosition(),0);//ToDo set arrays
+                    dismiss();
+                }
+
             }
         });
         btn_cancel=(Button)view.findViewById(R.id.btn_cancel_dialog);
@@ -80,14 +96,14 @@ public class DialogGetQty extends DialogFragment{
 
             }
         });
-        unit_spinner=(Spinner)view.findViewById(R.id.unit_spinner);
+
         unitSet=PlaceOrderSecond.getStringUnits();
         //ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,array); //selected item will look like a spinner set from XML
         final ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_spinner_item,PlaceOrderSecond.getStringUnits());
         spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         unit_spinner.setAdapter(spinnerArrayAdapter);
 
-        unit_demand_spinner=(Spinner)view.findViewById(R.id.demand_unit_spinner);
+
         unitSet=PlaceOrderSecond.getStringUnits();
         //ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,array); //selected item will look like a spinner set from XML
         final ArrayAdapter<String> spinnerArrayAdapter2 = new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_spinner_item,PlaceOrderSecond.getStringUnits());
@@ -96,16 +112,32 @@ public class DialogGetQty extends DialogFragment{
 
         return view;
     }
-    public int getUnitposition(String selection){
-        for (int i=0;i<unitSet.length;i++){
-            if (unitSet[i].equals(selection)){
-                return i;
-            }
-        }
-        return -1;
-    }
 
-    interface Communicator{
+public void show_warning(){
+    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+    builder
+            .setTitle("Quantity limit exceed")
+            .setMessage("Press yes if you want to revise input to demand list. Otherwise Select No(Only remain will bill)")
+            .setIcon(android.R.drawable.ic_dialog_alert)
+            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    //Yes button clicked, do something
+                    txt_get_qty.setText(String.valueOf(PlaceOrderSecond.cur_st_value));
+                    txt_get_dmnd_qty.setText(String.valueOf(entered_qty-PlaceOrderSecond.cur_st_value));
+                }
+            })
+            .setNegativeButton("No", new DialogInterface.OnClickListener(){
+
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    cm.onDialogMessage(PlaceOrderSecond.cur_st_value,0,unit_spinner.getSelectedItemPosition(),0);//ToDo set arrays
+                    dismiss();
+                }
+            })						//Do nothing on no
+            .show();
+
+}
+interface Communicator{
         public void onDialogMessage(int qty,int demandQty,int qtyUnitindex,int demandQtyUnitIndex);
     }
 //ToDo change number of parameteres in onDialogMessage method (to get qty in dif units),get demand qty,date for demanded values
